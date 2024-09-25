cfd9a407adb5cb475dbc769d6e36c5fe59ea9395
luc_jaubert <j.luc@obvy-app.com>
2024-09-25 14:26:17 +0200

    suite effects pahe homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { SeoService } from '../../services/seo.service';
 
 @Component({
@@ -15,6 +17,8 @@ export class HomepageComponent implements OnInit {
   constructor(private seoService: SeoService) {}
 
   ngOnInit() {
+    gsap.registerPlugin(ScrollTrigger);
+
     this.seoService.setMetaTitle("Anglais du Vin - Kathryn Walton Ward");
     this.seoService.setMetaDescription("Découvrez 'Anglais du Vin' par Kathryn Walton Ward, le manuel définitif pour maîtriser l'anglais dans le secteur viticole.");
     this.seoService.updateMetaTag('keywords', 'Anglais du Vin, Kathryn Walton Ward, éducation vin, manuel viticulture, vocabulaire viticole, formation sommelier');
@@ -33,5 +37,61 @@ export class HomepageComponent implements OnInit {
   scrollToAdditionalInfo(): void {
     const detailsSection = document.getElementById('additional-info-section');
     detailsSection?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  ngAfterViewInit(): void {
+    gsap.fromTo(".mt-8", 
+      { opacity: 0, y: 50 }, 
+      { opacity: 1, y: 0, duration: 1.5, ease: "power2.out", scrollTrigger: {
+        trigger: ".mt-8",
+        start: 'top 80%',
+        toggleActions: 'play none none none',
+      }}
+    );
+
+    gsap.fromTo(".subtitle", 
+      { opacity: 0, y: 50 }, 
+      { opacity: 1, y: 0, duration: 1.5, ease: "power2.out", scrollTrigger: {
+        trigger: ".subtitle",
+        start: 'top 80%',
+        toggleActions: 'play none none none',
+      }}
+    );
+
+    gsap.fromTo(".press-link span", 
+      { opacity: 0, y: 0 }, 
+      { opacity: 1, y: 0, stagger: 0.1, duration: 1.5, ease: "power2.out", scrollTrigger: {
+        trigger: ".press-link",
+        start: 'top 80%',
+        toggleActions: 'play none none none',
+      }}
+    );
+
+    gsap.fromTo(".book-cover-container, .book-details, .col-md-7", 
+      { opacity: 0, y: 100 }, 
+      { opacity: 1, y: 0, duration: 1.5, ease: "power2.out", scrollTrigger: {
+        trigger: ".book-cover-container",
+        start: 'top 80%',
+        toggleActions: 'play none none none',
+      }}
+    );
+
+    gsap.fromTo(".info-author, .note-content, .img-fluid-note", 
+      { opacity: 0, y: 50 }, 
+      { opacity: 1, y: 0, stagger: 0.2, duration: 1.5, ease: "power2.out", scrollTrigger: {
+        trigger: "#additional-info-section",
+        start: 'top 80%',
+        toggleActions: 'play none none none',
+      }}
+    );
+
+    gsap.fromTo(".author-info-additionnal, .img-fluid-author", 
+      { opacity: 0, y: 100 }, 
+      { opacity: 1, y: 0, duration: 1.5, ease: "power2.out", scrollTrigger: {
+        trigger: ".author-info-additionnal",
+        start: 'top 80%',
+        toggleActions: 'play none none none',
+      }}
+    );
   }
 }
--- src/app/pages/press/press.component.ts
+++ src/app/pages/press/press.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-press',
@@ -9,11 +11,53 @@ import { RouterModule } from '@angular/router';
   standalone: true, 
   imports: [CommonModule, RouterModule]
 })
-export class PressComponent implements OnInit {
+export class PressComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
   }
 
+  ngAfterViewInit(): void {
+    const pressBoxes = document.querySelectorAll('.press-box');
+
+    pressBoxes.forEach((box, index) => {
+      const isLeft = index % 2 === 0;
+
+      // Animation pour les images
+      gsap.fromTo(box.querySelector('.press-image-container'), 
+        {
+          x: isLeft ? -200 : 200,  // Position initiale à gauche ou à droite
+          opacity: 0  // Masquer l'image avant l'animation
+        }, 
+        {
+          x: 0,  // Arrive à sa position normale
+          opacity: 1,  // Devenir visible
+          duration: 1,  // Durée de l'animation
+          scrollTrigger: {
+            trigger: box,
+            start: 'top 80%',
+            toggleActions: 'play none none none',
+          }
+        });
+
+      // Animation pour le texte
+      gsap.fromTo(box.querySelector('.press-text-container'), 
+        {
+          y: 100,  // Le texte arrive du bas
+          opacity: 0
+        }, 
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          scrollTrigger: {
+            trigger: box,
+            start: 'top 80%',
+            toggleActions: 'play none none none',
+          }
+        });
+    });
+  }
 }
